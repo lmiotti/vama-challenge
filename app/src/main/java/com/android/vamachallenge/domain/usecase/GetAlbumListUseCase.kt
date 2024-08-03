243e68875dbedf5f4c55931b3f4514bc7f3e74a7
Lucas Miotti <lucasmiotti@MacBook-Pro-de-Lucas.local>
2024-08-03 11:20:03 -0300

    Fix on flows dispatchers

--- app/src/main/java/com/android/vamachallenge/domain/usecase/GetAlbumListUseCase.kt
+++ app/src/main/java/com/android/vamachallenge/domain/usecase/GetAlbumListUseCase.kt
@@ -1,8 +1,11 @@
 package com.android.vamachallenge.domain.usecase
 
 import com.android.vamachallenge.data.repository.album.AlbumRepository
+import com.android.vamachallenge.di.DefaultDispatcher
+import com.android.vamachallenge.di.IoDispatcher
 import com.android.vamachallenge.domain.model.Album
 import com.android.vamachallenge.domain.model.Resource
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
@@ -10,11 +13,14 @@ import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetAlbumListUseCase @Inject constructor(
-    private val albumRepository: AlbumRepository
+    private val albumRepository: AlbumRepository,
+    @IoDispatcher private val iOdispatcher: CoroutineDispatcher,
+    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
 ) {
 
     suspend operator fun invoke(): Flow<Resource<List<Album>>> {
         return albumRepository.getAlbums()
+            .flowOn(iOdispatcher)
             .map {
                 when(it) {
                     is Resource.Success -> {
@@ -25,6 +31,6 @@ class GetAlbumListUseCase @Inject constructor(
                     is Resource.Loading -> Resource.Loading()
                 }
             }
-            .flowOn(Dispatchers.IO)
+            .flowOn(defaultDispatcher)
     }
 }
